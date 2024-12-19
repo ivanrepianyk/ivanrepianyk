b7f86c7d6e9cb242fcb7e074ea968dddf18e1f3d
ivanrepianyk <ivanrepianyk@college.udhtu.edu.ua>
2024-12-19 18:04:38 +0200

    Add files via upload

---
+++ task4/index.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: "Іван",
+    lastName: "Реп’яшник",
+    age: 30
+};
+
+person.email = "ivanrepianyk@college.udhtu.edu.ua";
+
+delete person.age;
+
+console.log(person);
